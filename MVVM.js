dfb0d24d225a056299467057e82417d2dae7ce65
Xiaoqianerli <1259688240@qq.com>
2023-02-08 23:43:39 +0800

    optimized code

--- MVVM.js
+++ MVVM.js
@@ -2,38 +2,34 @@ let obj = {
     name: "123",
     company: "321",
   };
-
-  const fnArr = new Set();
-  obj = new Proxy(obj, {
-    get: function (target, propKey, receiver) {
-      console.log(`getting ${propKey}!`);
-      fnArr.add(watch);
-      return Reflect.get(target, propKey, receiver);
-    },
-    set: function (target, propKey, value, receiver) {
-      console.log(`setting ${propKey} ${value}!`);
-      target[propKey] = value;
-      fnArr.forEach((fn) => fn());
-      return true;
-    },
-  });
+  
   // const templatehtml = document.body.innerHTML;
-  function watch() {
+  function watch(object) {
     // Todo
-    // document.body.innerHTML = templatehtml.replace(/{{(.+?)}}"></g, (...rest) => {
-    //   console.log('==>', rest)
-    //   return rest[0] = `${obj[rest[1]]}">${obj[rest[1]]}<`
-    // })
-    document.body.innerHTML = document.body.innerHTML.replace(
-      /(?<="{{(.+?)}}")>.*</g,
-      (...rest) => {
-        console.log("==>", rest);
-        return (rest[0] = `>${obj[rest[1]]}<`);
-      }
-    );
+    obj = new Proxy(object, {
+        get: function (target, propKey, receiver) {
+          console.log(`getting ${propKey}!`);
+          return Reflect.get(target, propKey, receiver);
+        },
+
+        set: function (target, propKey, value, receiver) {
+          console.log(`setting ${propKey} ${value}!`);
+          target[propKey] = value;
+
+          document.body.innerHTML = document.body.innerHTML.replace(
+            /(?<="{{(.+?)}}")>.*</g,
+            (...rest) => {
+              console.log("==>", rest);
+              return (rest[0] = `>${target[rest[1]]}<`);
+            }
+          );
+          return true;
+        },
+    });
+    
   }
 
-  watch();
+  watch(obj);
 
   obj.company = "h";
 
